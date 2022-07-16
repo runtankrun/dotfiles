741a3346f7a6d300bf50f237d769bd72043b8609
runtankrun <emelli@protonmail.com>
2022-07-16 13:02:38 -0400

    minor update

--- alis/download.sh
+++ alis/download.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
 set -eu
 set -o xtrace
-loadkeys us
 
 decrypt(){
     openssl enc -aes-256-cbc -a -A -d -md sha512 -pbkdf2 -iter 250000 -salt $@
 }
 
 url='https://github.com/runtankrun/dotfiles/raw/dev/alis/alis.tar.gz'
-curl -L -O $url
+curl -sL -O $url
 
-tar -xvf alis.tar.gz
+tar -xzvf alis.tar.gz
 chmod +x configs/*.sh
 chmod +x *.sh
 
