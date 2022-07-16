fb22b8a3781b442440c16a7e775d3c1056d954e0
runtankrun <emelli@protonmail.com>
2022-07-16 11:53:27 -0400

    unattended diaster recovery

---
+++ alis/download.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+set -o xtrace
+loadkeys us
+
+decrypt(){
+    openssl enc -aes-256-cbc -a -A -d -md sha512 -pbkdf2 -iter 250000 -salt $@
+}
+
+url='https://github.com/runtankrun/dotfiles/raw/recovery/alis/alis.tar.gz'
+curl -L -O $url
+
+tar -xvf alis.tar.gz
+chmod +x configs/*.sh
+chmod +x *.sh
+
+decrypt -in alis.dat -out alis.conf
