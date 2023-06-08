d698c3061c4fc09f2c76c9a69ca288c26945e80d
runtankrun <emelli@protonmail.com>
2023-06-07 21:34:33 -0400

    minor update

--- install.sh
+++ install.sh
@@ -3,15 +3,44 @@
 set -eu
 set -o xtrace
 
-decrypt(){
-    openssl enc -aes-256-cbc -a -A -d -md sha512 -pbkdf2 -iter 250000 -salt $@
+working_dir='installation_files'
+url='https://github.com/runtankrun/dotfiles/releases/download/v0.0.2/install.tar.gz'
+
+password_setup(){
+        echo "Enter a $1 password:"
+        read -s password
+        echo
+
+        echo "Confirm your $1 password:"
+        read -s password_confirm
+        echo
+
+        if [ "$password" != "$password_confirm" ]; then
+                echo "Passwords do not match. Please run the script again."
+                exit 1
+        fi
+
+        escaped_password=$(printf '%q' "$password")
+
+        sed -i "s/<$1_PASSWORD>/$escaped_password/g" installation_files/alis.conf
+        cat installation_files/alis.conf | grep $1
 }
 
-url='https://github.com/runtankrun/dotfiles/releases/download/v0.0.1/install.tar.gz'
-curl -sL -O $url
+build(){
+    rm -rf "$working_dir" && mkdir "$working_dir"
 
-tar -xzvf install.tar.gz
-chmod +x configs/*.sh
-chmod +x *.sh
+    curl -sL -O "$url"
+    tar -xvf install.tar.gz -C "$working_dir"
+    
+    chmod +x $working_dir/configs/*.sh
+    chmod +x $working_dir/*.sh
+}
 
-decrypt -in alis.dat -out alis.conf
+clean(){
+    rm install.tar.gz
+}
+
+build
+password_setup ROOT
+password_setup USER
+clean
